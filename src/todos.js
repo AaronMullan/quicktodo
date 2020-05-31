8789e742f144bb3354d0673a3d0bff85f944a475
AaronMullan <aaroncmullan@gmail.com>
2020-05-31 16:44:28 -0700

    need to be faster

--- src/todos.js
+++ src/todos.js
@@ -2,13 +2,45 @@ import React, { useState } from 'react';
 
 function Todos() {
   const [todos, setTodos] = useState(
-    [todos]
+    ['body moving','great balls of fire', 'hello darkness']
   )
+  const [value, setValue] = useState()
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTodo(value)
+    console.log(todos)
+  }
+  
+  const addTodo = (todo) => {
+    const newTodos = [...todos, todo]
+    setTodos(newTodos)
+  }
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+}
+
   return(
     <>
-  <form>
-  <input type="text" name="" id=""/>
-  <input type="button" value="add todo"onClick = { setTodos }/>
+    <ul>
+    {todos.map((element, index) => 
+    <li key={index}>{element}
+    <button onClick={() => deleteTodo(index)}>delete</button>
+    </li>
+    )
+     }
+    </ul>
+  <form onSubmit={handleSubmit}>
+  <input 
+    type="text" 
+    name="" 
+    id="" 
+    value={value}
+    onChange={e => setValue(e.target.value)}
+    />
+    <input type="submit" value="Submit" />
+
     </form>
   </>
   );
